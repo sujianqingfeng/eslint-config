fa6f049fa1e4bdb4b7ca9809c01cbb144d44c950
hens <121116111@qq.com>
2022-08-13 21:21:15 +0800

    feat: update typescript rule

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -4,9 +4,19 @@ module.exports = {
   extends: ['@sujian/basic', 'plugin:@typescript-eslint/recommended'],
   overrides: basic.overrides,
   rules: {
+    '@typescript-eslint/no-this-alias': [
+      'error',
+      {
+        'allowDestructuring': false, // Disallow `const { props, state } = this`; true by default
+        'allowedNames': ['self', 'that'] // Allow `const self = this`; `[]` by default
+      }
+    ],
+
+    'indent': 'off',
     '@typescript-eslint/indent': ['error', 2,],
 
     // off
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off'
   }
 }
