b4240ef05081cd767a6a5ff5a226911db7da078c
hens <121116111@qq.com>
2022-09-17 20:32:27 +0800

    feat: add vue3 rule

--- README.md
+++ README.md
@@ -7,6 +7,19 @@
 
 ## usage
 
+### vue3
+```
+pnpm i @sujian/eslint-config-vue3 -D
+```
+
+.eslintrc.json
+
+```json
+{
+  "extends": "@sujian/vue3"
+}
+```
+
 ### vue2
 ```
 pnpm i @sujian/eslint-config-vue2 -D
---
+++ packages/vue3/index.js
@@ -0,0 +1,25 @@
+const basic = require('@sujian/eslint-config-basic')
+
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: [
+    '@sujian/basic',
+    '@sujian/prettier',
+    '@sujian/vue2',
+    'plugin:vue/recommended',
+  ],
+  overrides: basic.overrides,
+  rules: {
+    // 0 是 off  1 是 warning  2 是 error
+    
+    // sfc 文件tag顺序
+    'vue/component-tags-order': [
+      2,
+      {
+        order: ['script', 'template', 'style']
+      }
+    ],
+  }
+}
